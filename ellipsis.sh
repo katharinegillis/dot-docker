fdb713d7eb4d76c5d82e57f808bef18c808a6a99
Katharine Cordes <gillis.katharine@gmail.com>
2020-03-27 23:07:38 -0700

    Install docker and configure

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,24 +1,55 @@
 #!/usr/bin/env bash
-#
-# katharinegillis/docker ellipsis package
 
-# The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+    pkg.pull
+}
 
-# pkg.push() {
-#     git.push
-# }
+pkg.pull() {
+    echo -e "\e[32mUpdating docker...\e[0m"
 
-# pkg.pull() {
-#     git.pull
-# }
+    # Install docker
+    docker_install
 
-# pkg.installed() {
-#     git.status
-# }
-#
-# pkg.status() {
-#     git.diffstat
-# }
+    fs.link_files files
+
+    echo -e "\e[32mDone docker.\e[0m"
+}
+
+docker_install() {
+    # Install Docker's package dependencies.
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+
+    # Download and add Docker's official public PGP key.
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    # Verify the fingerprint.
+    sudo apt-key fingerprint 0EBFCD88
+
+    # Add the `stable` channel's Docker upstream repository.
+    #
+    # If you want to live on the edge, you can change "stable" below to "test" or
+    # "nightly". I highly recommend sticking with stable!
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+    # Update the apt package list (for the new apt repo).
+    sudo apt-get update -y
+
+    # Install the latest version of Docker CE.
+    sudo apt-get install -y docker-ce
+
+    # Allow your user to access the Docker CLI without needing root access.
+    sudo usermod -aG docker $USER
+
+    # Install Python and PIP.
+    sudo apt-get install -y python python-pip
+
+    # Install Docker Compose into your user's home directory.
+    pip install --user docker-compose
+}
