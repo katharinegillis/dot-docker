cc707a319c05075173f6cb8f662df37d8f44fd65
Katie Cordes <gillis.katharine@gmail.com>
2020-08-08 17:23:16 -0700

    Update for wsl 2 and Ubuntu 20.04

--- run.sh
+++ run.sh
@@ -32,6 +32,6 @@ sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USER
 
 # Install docker-compose
-sudo apt-get install jq
+sudo apt-get install -y jq
 sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
 sudo chmod +x /usr/local/bin/docker-compose
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Uninstall Docker Compose
-pip uninstall -y docker-compose
+sudo rm -rf /usr/local/bin/docker-compose
 
 # Uninstall Docker CE
-sudo apt-get remove -y docker-ce
+sudo apt-get remove -y docker-ce jq
 
 # We will not remove the supporting programs in case another package uses them
