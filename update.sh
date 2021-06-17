cd226f002280acff3e32d7c76e1ff18b22d5ed05
Katharine Gillis <kgillis@fieldsafesolutions.com>
2021-06-17 12:06:02 -0600

    feat: add docker dangling image cleaning

--- update.sh
+++ update.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # Install the latest version of Docker CE.
-sudo apt-get install -y docker-ce
+#sudo apt-get install -y docker-ce
 
 # Install docker-compose
-sudo apt-get install -y jq
-sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo apt-get install -y jq
+#sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
+#sudo chmod +x /usr/local/bin/docker-compose
+
+# Ask about cleaning dangling images
+echo "Would you like to clean up dangling images? [y/n]"
+read -r answer
+if [ "$answer" == "yes" ] || [ "$answer" == "y" ]; then
+  "$HOME/bin/docker-clean"
+else
+  echo "Skipping cleaning of dangling images - if you wish to clean them manually, run the docker-clean script."
+fi
