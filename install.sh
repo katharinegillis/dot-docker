8578da80a313329a4fba60b6b0864ccaa2783d59
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-13 23:43:06 -0600

    refactor: remove non-Linux logic from package

--- install.sh
+++ install.sh
@@ -1,57 +1,41 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
+# Install Docker's package dependencies.
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
+# Download and add Docker's official public PGP key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-if [ "$SYSTEM" == "wsl" ]; then
-    choco.exe install docker-desktop -y
-    touch .restart.lock
-    exit
-elif [ "$SYSTEM" == "mac" ]; then
-    brew install --cask docker
-    open /Applications/Docker.app
-    echo "Please follow the prompts to get Docker Desktop running, then press any key to continue."
-    read -r var
-fi
+# Verify the fingerprint.
+sudo apt-key fingerprint 0EBFCD88
 
-if [ "$SYSTEM" != "mac" ]; then
-    # Install Docker's package dependencies.
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common
+# Add the `stable` channel's Docker upstream repository.
+#
+# If you want to live on the edge, you can change "stable" below to "test" or
+# "nightly". I highly recommend sticking with stable!
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
 
-    # Download and add Docker's official public PGP key.
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Update the apt package list (for the new apt repo).
+sudo apt-get update -y
 
-    # Verify the fingerprint.
-    sudo apt-key fingerprint 0EBFCD88
+# Install the latest version of Docker CE.
+sudo apt-get install -y docker-ce
 
-    # Add the `stable` channel's Docker upstream repository.
-    #
-    # If you want to live on the edge, you can change "stable" below to "test" or
-    # "nightly". I highly recommend sticking with stable!
-    sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+# Allow your user to access the Docker CLI without needing root access.
+sudo groupadd docker
+sudo usermod -aG docker "${USER}"
 
-    # Update the apt package list (for the new apt repo).
-    sudo apt-get update -y
+# Install docker-compose
+sudo apt-get install -y jq
+sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
+sudo chmod +x /usr/local/bin/docker-compose
 
-    # Install the latest version of Docker CE.
-    sudo apt-get install -y docker-ce
-
-    # Allow your user to access the Docker CLI without needing root access.
-    sudo groupadd docker
-    sudo usermod -aG docker "${USER}"
-
-    # Install docker-compose
-    sudo apt-get install -y jq
-    sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
-    sudo chmod +x /usr/local/bin/docker-compose
-
-    # Run the installer a second time to force it to get the docker service up and running (issue on Ubuntu)
-    sudo apt-get install -y docker-ce
-fi
+# Run the installer a second time to force it to get the docker service up and running (issue on Ubuntu)
+sudo apt-get install -y docker-ce
--- update.sh
+++ update.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
 
-if [ "$SYSTEM" == "wsl" ]; then
-    echo "Please upgrade via Docker Desktop instead of Chocolatey."
-elif [ "$SYSTEM" == "mac" ]; then
-    brew upgrade --cask docker
-fi
+# Install the latest version of Docker CE.
+sudo apt-get install -y docker-ce
 
-if [ "$SYSTEM" != "mac" ]; then
-    # Install the latest version of Docker CE.
-    sudo apt-get install -y docker-ce
-
-    # Install docker-compose
-    sudo apt-get install -y jq
-    sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
-    sudo chmod +x /usr/local/bin/docker-compose
-fi
+# Install docker-compose
+sudo apt-get install -y jq
+sudo curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq --arg PLATFORM_ARCH "$(echo `uname -s`-`uname -m`)" -r '.assets[] | select(.name | endswith($PLATFORM_ARCH)).browser_download_url' | xargs sudo curl -L -o /usr/local/bin/docker-compose --url
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Ask about cleaning dangling images
 echo "Would you like to clean up docker? [y/n]"
@@ -24,5 +15,5 @@ read -r answer
 if [ "$answer" == "y" ]; then
   "$HOME/bin/docker-clean"
 else
-  echo "Skipping cleaning of ocker - if you wish to clean it manually, run the docker-clean script."
+  echo "Skipping cleaning of docker - if you wish to clean it manually, run the docker-clean script."
 fi
